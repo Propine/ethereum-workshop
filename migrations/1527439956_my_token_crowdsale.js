c85fc92efeec2382e2780f5794a43a11a53d137f
dolita <dolita.yap@gmail.com>
2018-06-10 21:21:20 +0800

    Dynamic openingTime

--- migrations/1527439956_my_token_crowdsale.js
+++ migrations/1527439956_my_token_crowdsale.js
@@ -2,7 +2,7 @@ const MyTokenCrowdsale = artifacts.require('./MyTokenCrowdsale.sol');
 const MyToken = artifacts.require('./MyToken.sol');
 
 module.exports = function(deployer, network, accounts) {
-  const openingTime = 1527469032 + 86400; // web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
+  const openingTime = Math.round((new Date(Date.now() + 180000).getTime())/1000);  // 180000 ms = 3 mins in future
   const closingTime = openingTime + 86400 * 20; // 20 days
   const rate = new web3.BigNumber(1);
   const wallet = accounts[1];
